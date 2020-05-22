a986aff78010f974f8e8c0b47466932ca41a5744
mkobayashime <m.kobayashi.me@gmail.com>
2020-05-22 16:43:07 +0900

    v1.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TWINS Enhanced
 // @namespace    https://twins.tsukuba.ac.jp/
-// @version      0.1
+// @version      1.0
 // @description  Browser userscript to improve semantics of TWINS
 // @author       @m_kobayashi_me  https://twitter.com/m_kobayashi_me
 // @match        https://twins.tsukuba.ac.jp/*
@@ -10,4 +10,34 @@
 
 (function() {
   "use strict"
+
+  window.onload = () => {
+    const ids = [
+      "tab-home",
+      "tab-rs",
+      "tab-si",
+      "tab-gk",
+      "tab-kh",
+      "tab-en",
+      "tab-kj",
+      "portalsmart",
+      "portallocale",
+      "portaltimer",
+      "logout"
+    ]
+
+    for (const id of ids) {
+      const originalDiv = document.getElementById(id)
+      originalDiv.style.position = "relative"
+
+      const domA = document.createElement("a")
+      domA.style.position = "absolute"
+      domA.style.width = "100%"
+      domA.style.height = "100%"
+      domA.style.top = "0"
+      domA.style.left = "0"
+
+      originalDiv.appendChild(domA)
+    }
+  }
 })();
